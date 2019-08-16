02714d46d5cdb3865fa2ba4b370e1d5d20ff1fdb
Eyal Ganor <eyalganor@gmail.com>
2019-08-16 13:03:11 +0300

    /eGan  'this is version one'  line 8 of ui.js changed to <ul>  no validations yet
    /eGan  'this is version one'
    
    line 8 of ui.js changed to <ul>
    
    no validations yet

--- test.js
+++ test.js
@@ -1,23 +1,24 @@
 test();
+
 function test() {
 
-  // create new shows
-  createShow('israel gig', [31.771959, 35.217018], new Date());
-  createShow('new york fun', [40.7128, 74.0060], new Date());
-  createShow('san fran fun', [37.7749, 122.4194], new Date());
+    // create new shows
+    // createShow('israel gig', [31.771959, 35.217018], new Date());
+    // createShow('new york fun', [40.7128, 74.0060], new Date());
+    // createShow('san fran fun', [37.7749, 122.4194], new Date());
 
-  // delete a show
-  deleteShow(2);
-  
-  // update a show
-  var paramters = {date: 'new dateeeee',description: 'updated description'};
-  updateShow(paramters, 1);
+    // delete a show
+    // deleteShow(2);
 
-  // render shows in a list
-  renderShows(state.shows);
+    // update a show
+    // var paramters = { showLocation: 'london', description: 'updated description' };
+    // updateShow(paramters, 1);
 
-  console.table(state.shows);
+    // render shows in a list
+    // renderShows(state.shows);
 
-  // openDialog('mock title', 'mock content');
-  // closeDialog();
+    // console.table(state.shows);
+
+    // openDialog('mock title', 'mock content');
+    // closeDialog();
 }
